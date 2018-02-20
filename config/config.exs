fa0f4e8f13df3ef85483eac9ccd47c974d2e6ee1
Daniel Morgan <danmorgz@gmail.com>
2018-02-20 10:02:17 +0000

    guardian auth

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,12 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+  #Configure Guardian
+  config :instagram, InstagramWeb.Auth.Guardian,
+       issuer: "instagram",
+       verify_issuer: true,
+       secret_key: "tViWXUjcQEwr+NPupTmcyyQsSmo12vlA5GMSGdpEL/OVUPdgNX3xywqZHTfneZja"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
