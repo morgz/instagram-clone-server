60a6db4b13c01e48093cf1b782352d42049aae29
Daniel Morgan <danmorgz@gmail.com>
2018-02-28 10:32:39 +0000

    remove ngrok

--- config/config.exs
+++ config/config.exs
@@ -15,27 +15,27 @@ config :instagram, InstagramWeb.Endpoint,
   secret_key_base: "xq7laVJaqZ/gTquWFIdZmPrXd4ONDXkPOKdA2ZzzLXTRgnylQxy9cjzn6w5heh/R",
   render_errors: [view: InstagramWeb.ErrorView, accepts: ~w(json)],
   pubsub: [name: Instagram.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-  #Configure Guardian
-  config :instagram, InstagramWeb.Auth.Guardian,
-       issuer: "instagram",
-       verify_issuer: true,
-       secret_key: "tViWXUjcQEwr+NPupTmcyyQsSmo12vlA5GMSGdpEL/OVUPdgNX3xywqZHTfneZja"
+#Configure Guardian
+config :instagram, InstagramWeb.Auth.Guardian,
+      issuer: "instagram",
+      verify_issuer: true,
+      secret_key: "tViWXUjcQEwr+NPupTmcyyQsSmo12vlA5GMSGdpEL/OVUPdgNX3xywqZHTfneZja"
 
-  config :ex_aws,
-    access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-    secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
-    s3: [
-      scheme: "https://",
-      host: "instagram-clone-morgz.s3.amazonaws.com",
-      region: "us-east-1"
-    ]
+config :ex_aws,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  s3: [
+    scheme: "https://",
+    host: "instagram-clone-morgz.s3.amazonaws.com",
+    region: "us-east-1"
+  ]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
